6623f2bb303b1fa56293bd0fbc7eaf43b5a557be
UppercutsFromKupchino <mitya.kalmykov.01@mail.ru>
2022-02-14 06:44:14 +0300

    zaebalo

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 class Config:
     dialect = 'postgresql'
-    username = 'mrulfwmtmwplos'
-    password = 'a67e9ca8e6e41a6d81c1df0a49bd9472618d91f098251b780a12776e86e83511'
-    host = 'ec2-52-211-158-144.eu-west-1.compute.amazonaws.com'
-    db_name = 'dbgsg3c5be3uat'
+    username = 'oypoardysbypop'
+    password = '8bf53d8f8de04adf1175ba1c4504c1d36eb483728ed320e5c0b27dff5ac7ba6a'
+    host = 'ec2-54-78-36-245.eu-west-1.compute.amazonaws.com'
+    db_name = 'dftdt7n2jm2hre'
 
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
